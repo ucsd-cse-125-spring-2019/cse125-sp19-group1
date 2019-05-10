43db82e57784d6e802fd27bf94f738ebd5f06c5d
Sleightly <ejc042@ucsd.edu>
2019-05-09 17:55:09 -0700

    adding new clients

--- network/ServerGame.cpp
+++ network/ServerGame.cpp
@@ -709,10 +709,7 @@ void ServerGame::resetGame()
 	for (iter = network->sessions.begin(); iter != network->sessions.end(); iter++)
 	{
 		Location loc = gameData->initLocs[count];
-		std::cout << loc.getX() << std::endl;
-		std::cout << loc.getY() << std::endl;
-		std::cout << loc.getZ() << std::endl;
-		gameData->getPlayer(iter->first)->setLocation(loc.getX(), loc.getY(), loc.getZ());
+		gameData->addNewClient(iter->first, loc);
 		count++;
 	}
 	std::cout << "GAME RESET" << std::endl;
