76c8d67953c58a4d81fd8e321b9533a5c14b6eb5
Edward Chen <edwjchen@gmail.com>
2020-05-01 11:09:43 -0700

    adding in new client for graphics

--- network/ClientNetwork.h
+++ network/ClientNetwork.h
@@ -18,7 +18,7 @@ extern char serverIpAddress[64];
 // size of our buffer
 #define DEFAULT_BUFLEN 512
 // port to connect sockets through 
-#define DEFAULT_PORT "6881"
+#define DEFAULT_PORT "27015"
 // Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
 #pragma comment (lib, "Ws2_32.lib")
 #pragma comment (lib, "Mswsock.lib")
